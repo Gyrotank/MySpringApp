43bccf8b331baaa28b3acea69640d8f70dcbc263
Gyrotank <glomozda.dm@gmail.com>
2015-03-31 00:25:21 +0300

    commit

---
+++ src/test/java/ua/epam/rd/service/AppTest.java
@@ -0,0 +1,14 @@
+package ua.epam.rd.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest    
+{
+    @Test
+	public void testApp()
+    {
+        assertTrue( true );
+    }
+}
